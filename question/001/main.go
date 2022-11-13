d111aa34e7e132f1b6999d94a8e8e4e0aca04360
unagi3107 <urano0717@gmail.com>
2022-11-13 11:25:15 +0900

    add question

---
+++ question/001/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var input string
+	fmt.Println("みかんの個数を入力してください。")
+	var sc = bufio.NewScanner(os.Stdin)
+	sc.Scan()
+
+	input = sc.Text()
+
+	orangeNum, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("fail to convert int.")
+		return
+	}
+
+	if orangeNum <= 1 && orangeNum >= 100 {
+		fmt.Println("number of oranges is incorrect.")
+		return
+	}
+
+	appleNum := 5
+
+	total := appleNum + orangeNum
+
+	fmt.Printf("りんごとみかんの合計:%d", total)
+}
