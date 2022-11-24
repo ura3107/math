47a3404240fea02a687235b50dcdf14d2ab160bd
unagi3107 <urano0717@gmail.com>
2022-11-24 23:28:03 +0900

    spigot modify

--- spigot/main.go
+++ spigot/main.go
@@ -2,21 +2,22 @@ package main
 
 import "fmt"
 
-var base int = 10000    // 基底
-var n int = 8400        // 計算項数
-var temp int            // 一時変数/繰り上がり
-var out int             // 出力値
-var denom int           // 分母
-var numerator [8400]int // 分子
+var base int = 10000 // 基底
 
 func main() {
+	var numerator [8400]int // 分子
+	base := 10000           // 基底
+	n := 8400               // 計算項数
+
 	for i := 0; i < n; i++ {
 		numerator[i] = base / 5
 	}
 
-	out = 0
+	var denom int
+	out := 0
+
 	for n = 8400; n > 0; n -= 14 {
-		temp = 0
+		temp := 0
 		for i := n - 1; i > 0; i-- {
 			denom = 2*i - 1
 			temp = temp*i + numerator[i]*base
